53351785c28bc50a38df8c345badc1e4971ca8dd
9paramraol <53121415+9paramraol@users.noreply.github.com>
2020-12-02 23:27:51 +0530

    first file to get the distinct queries

---
+++ src/government scheme analysis/distinct_querie.py
@@ -0,0 +1,26 @@
+import json
+
+with open('./data.json') as f:
+  data_dict = json.load(f)
+
+
+govt_queries = []
+dummy_set = {""}
+for yr in data_dict:
+	for month in data_dict[yr]:
+		for state in data_dict[yr][month]:
+			for dist in data_dict[yr][month][state]:
+				temp_list = data_dict[yr][month][state][dist]
+				for elem in temp_list:
+					cat = elem[3]
+					query = elem[4]
+					if (cat=="Government Schemes"):
+						if(query not in dummy_set):
+							govt_queries.append(query)
+							dummy_set.add(query)
+
+
+govt_queries.sort()
+with open('distinct_govt_query', 'w') as f:
+    for item in govt_queries:
+        f.write("%s\n" % item)
