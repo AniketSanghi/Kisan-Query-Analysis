e08f3ab0f9a97f054b0704a6afd1fae8db9f85d6
9paramraol <53121415+9paramraol@users.noreply.github.com>
2020-12-06 14:47:51 +0530

    final directories.sh

--- src/government scheme analysis/directories.sh
+++ src/government scheme analysis/directories.sh
@@ -1,24 +1,52 @@
+
+if [ ! -d ./hindi_tranlations ]; then
+  mkdir -p ./hindi_tranlations;
+fi
+
 if [ ! -d ./preprocessing ]; then
   mkdir -p ./preprocessing;
+fi
+
+
+if [ ! -d ./preprocessing ]; then
+  mkdir -p ./preprocessing;
+fi
+
+if [ ! -d ./preprocessing/batches ]; then
+  mkdir -p ./preprocessing/batches;
+fi
+
+if [ ! -d ./fasal\ bima ]; then
+  mkdir -p ./fasal\ bima;
 fi
 
 if [ ! -d ./fasal\ bima/output ]; then
   mkdir -p ./fasal\ bima/output;
 fi
 
+if [ ! -d ./kcc ]; then
+  mkdir -p ./kcc;
+fi
+
 if [ ! -d ./kcc/output ]; then
   mkdir -p ./kcc/output;
+fi
+
+if [ ! -d ./samman\ nidhi ]; then
+  mkdir -p ./samman\ nidhi;
 fi
 
 if [ ! -d ./samman\ nidhi/output ]; then
   mkdir -p ./samman\ nidhi/output;
 fi
 
+if [ ! -d ./mandhan ]; then
+  mkdir -p ./mandhan;
+fi
+
 if [ ! -d ./mandhan/output ]; then
   mkdir -p ./mandhan/output;
 fi
 
-if [ ! -d ./preprocessing/batches ]; then
-  mkdir -p ./preprocessing/batches;
-fi
+
 
