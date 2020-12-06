974250ceff3125b8c1c3f62b48112f04a2a1f967
9paramraol <53121415+9paramraol@users.noreply.github.com>
2020-12-06 11:26:15 +0530

    README + directory.sh
    readme and making the directory structure

---
+++ src/government scheme analysis/directories.sh
@@ -0,0 +1,20 @@
+if [ ! -d ./fasal\ bima/output ]; then
+  mkdir -p ./fasal\ bima/output;
+fi
+
+if [ ! -d ./kcc/output ]; then
+  mkdir -p ./kcc/output;
+fi
+
+if [ ! -d ./samman\ nidhi/output ]; then
+  mkdir -p ./samman\ nidhi/output;
+fi
+
+if [ ! -d ./mandhan/output ]; then
+  mkdir -p ./mandhan/output;
+fi
+
+if [ ! -d ./preprocessing/batches ]; then
+  mkdir -p ./preprocessing/batches;
+fi
+
